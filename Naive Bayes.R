5e69f2fb103017515b82f22a6cf7356a898082a8
sedale33 <36580609+sedale33@users.noreply.github.com>
2019-08-31 22:55:07 -0400

    correct P_hat dimension

--- Naive Bayes.R
+++ Naive Bayes.R
@@ -2,8 +2,9 @@
 require(ramify)
 require(R6)
 
-GaussNB <- R6Class("GaussNB", list( K = 0, mu = 0, cov = 0,
-  fit = function(X, y, epsilon= 1e-3){
+GaussNB <- R6Class("GaussNB", list( K = 0, mu = 0, cov = 0, y = 0,
+  fit = function(X, y, epsilon= 1e-2){
+    self$y <- y
     self$K <- unique(y)
     data <- cbind(X, y)
     nfeat <- length(data[1,]) - 1
@@ -13,7 +14,7 @@ GaussNB <- R6Class("GaussNB", list( K = 0, mu = 0, cov = 0,
     invisible(self)
     },
   perdict = function(X){
-    P_hat <- mat.or.vec(length(y), length(self$K))
+    P_hat <- mat.or.vec(length(X[,1]), length(self$K))
     for(i in seq_along(self$K)){
       for(r in seq_along(X[,1])){
         P_hat[r, i] <- sum(-0.5*log(2*pi) - 0.5*log((self$cov[i,-1])) - sum((X[r,]-self$mu[i,-1])^2)/(2*(self$cov[i,-1]^2)))
